03de5da9bfbe9cb02d5d422114ba322eb8d47419
Alex119 <alexmovrt@gmail.com>
2025-12-04 09:55:06 +0100

    Actualizacion rapida

---
+++ src/Matrices/RetoSumaDiagonalMatriz.java
@@ -0,0 +1,54 @@
+package Matrices;
+import java.util.*;
+public class RetoSumaDiagonalMatriz {
+    static Scanner teclado=new Scanner(System.in);
+
+public static void main(String[] args) {
+    /* matriz 3 x3 almacenar los valores secuencialñes a 100+ */
+
+    System.out.println("Dictar numero de renglones y posteriormente numero de columnas");
+int [][]matrizSumaDiagonal=new int[Integer.valueOf(teclado.nextLine())][Integer.valueOf(teclado.nextLine())];
+
+for (int i = 0; i < matrizSumaDiagonal.length; i++) {
+   
+    for (int j = 0; j < matrizSumaDiagonal[i].length; j++) {
+   System.out.println("Ingresar valor de renglon ["+i+"] y columna ["+j+"] :");
+   matrizSumaDiagonal[i][j]=Integer.valueOf(teclado.nextLine());
+   
+   
+
+    }
+}
+
+int sumaDiagonal=0;
+int sumaDiagonalContralado=0;
+for (int i = 0; i < matrizSumaDiagonal.length; i++) {
+    for (int j = 0; j < matrizSumaDiagonal[i].length; j++) {
+        
+        System.out.println(" Muestra de los valores dentro de la matriz :\n ["+i+"] ["+j+"] :"+matrizSumaDiagonal[i][j]);
+        if (i==j) {
+        System.out.println("Suma de todas las matrices de manera diagonal primera salida : "+(sumaDiagonal+=matrizSumaDiagonal[i][j]));
+        }
+}
+}
+
+for (int k = 0; k < matrizSumaDiagonal.length; k++) {
+    for (int l = 0; l < matrizSumaDiagonal[k].length; l++) {
+        
+System.out.println("\n Muestra de los valores dentro de la matriz :\n ["+k+"] ["+l+"] :"+matrizSumaDiagonal[k][l]);
+if (k+l==matrizSumaDiagonal[0].length-1) /*el [0]puede ser [k] tambien por si acaso para que lea columnas en vez de fila
+pero para la maquina o compilacion es menos optimizable*/{
+    System.out.println("Suma de todas las matrices de manera diagonal en contralado: "+(sumaDiagonalContralado+=matrizSumaDiagonal[k][l]));
+}
+
+
+    }
+}
+
+
+
+
+
+
+}
+}
