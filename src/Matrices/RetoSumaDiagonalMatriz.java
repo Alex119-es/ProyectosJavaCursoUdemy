996e33ea4aedf0f5dfbe184b26f3ac868a08708c
Alex119 <alexmovrt@gmail.com>
2025-12-04 13:59:26 +0100

    Orden de la matriz

--- src/Matrices/RetoSumaDiagonalMatriz.java
+++ src/Matrices/RetoSumaDiagonalMatriz.java
@@ -22,15 +22,19 @@ for (int i = 0; i < matrizSumaDiagonal.length; i++) {
 
 int sumaDiagonal=0;
 int sumaDiagonalContralado=0;
+
 for (int i = 0; i < matrizSumaDiagonal.length; i++) {
     for (int j = 0; j < matrizSumaDiagonal[i].length; j++) {
         
         System.out.println(" Muestra de los valores dentro de la matriz :\n ["+i+"] ["+j+"] :"+matrizSumaDiagonal[i][j]);
         if (i==j) {
-        System.out.println("Suma de todas las matrices de manera diagonal primera salida : "+(sumaDiagonal+=matrizSumaDiagonal[i][j]));
+            sumaDiagonal+=matrizSumaDiagonal[i][j];
+        System.out.println("\nIteracion de la suma numero ["+i+"] de manera diagonal primera salida : "+(sumaDiagonal));
         }
 }
 }
+        System.out.println("\nSuma de todas las matrices de manera diagonal primera salida : "+(sumaDiagonal));
+
 
 for (int k = 0; k < matrizSumaDiagonal.length; k++) {
     for (int l = 0; l < matrizSumaDiagonal[k].length; l++) {
@@ -38,13 +42,17 @@ for (int k = 0; k < matrizSumaDiagonal.length; k++) {
 System.out.println("\n Muestra de los valores dentro de la matriz :\n ["+k+"] ["+l+"] :"+matrizSumaDiagonal[k][l]);
 if (k+l==matrizSumaDiagonal[0].length-1) /*el [0]puede ser [k] tambien por si acaso para que lea columnas en vez de fila
 pero para la maquina o compilacion es menos optimizable*/{
-    System.out.println("Suma de todas las matrices de manera diagonal en contralado: "+(sumaDiagonalContralado+=matrizSumaDiagonal[k][l]));
+
+sumaDiagonalContralado+=matrizSumaDiagonal[k][l];
+
+    System.out.println("\nIteracion de la suma numero ["+k+"] de manera diagonal en contralado: "+(sumaDiagonalContralado));
 }
 
-
-    }
 }
 
+}
+
+System.out.println("\nSuma de todas las matrices de manera diagonal en contralado : "+(sumaDiagonalContralado));
 
 
 
