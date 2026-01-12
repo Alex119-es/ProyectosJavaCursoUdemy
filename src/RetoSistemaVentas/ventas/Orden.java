b4cd10556a44bddc77044e4416cc544fdacd205b
Alex119 <alexmovrt@gmail.com>
2026-01-12 13:10:29 +0100

    Reto Sistema Venta en proceso

---
+++ src/RetoSistemaVentas/ventas/Orden.java
@@ -0,0 +1,19 @@
+package RetoSistemaVentas.ventas;
+
+public class Orden {
+int productos[];
+
+void agregarProducto(String productoAñadir){
+    
+}
+
+
+int calcularTotal(){
+    
+}
+
+void mostrarOrden(){
+
+}
+
+}
---
+++ src/RetoSistemaVentas/ventas/Producto.java
@@ -0,0 +1,29 @@
+package RetoSistemaVentas.ventas;
+
+public class Producto {
+private int idProducto;
+private String nombre;
+private double precio;
+public int getIdProducto() {
+    return idProducto;
+}
+public String getNombre() {
+    return nombre;
+}
+public double getPrecio() {
+    return precio;
+}
+public void setIdProducto(int idProducto) {
+    this.idProducto = idProducto;
+}
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+public void setPrecio(double precio) {
+    this.precio = precio;
+}
+@Override
+public String toString() {
+    return "Producto [idProducto=" + idProducto + ", nombre=" + nombre + ", precio=" + precio + "]";
+}
+}
