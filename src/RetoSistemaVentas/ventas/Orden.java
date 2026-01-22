dc7948de04a582e95c361d38b678c94a91d0eea3
Alex119 <alexmovrt@gmail.com>
2026-01-22 10:33:29 +0100

    Actualizacion sistema ventas

--- src/RetoSistemaVentas/ventas/Orden.java
+++ src/RetoSistemaVentas/ventas/Orden.java
@@ -28,5 +28,42 @@ public class Orden {
         }
         
     }
-
+    public double calcularTotal(){
+        double total=0;
+for (int i = 0; i < this.contadorProductos/*se menciona contador porque se deberia basar mas en
+    la cantidad metida lo que contador porductos si tiene si usasemos productos fijamos con su max productos que es el limite 
+     */; i++) {
+    var producto=this.productos[i];/*si haces que tenga el valor de un objeto creado de la clase Producto
+    automaticamente esa variable que tiene referencia y resultado puede acceder a los metodos disponibles */
+    total+=producto.getPrecio();
 }
+return total;
+    }
+
+// NOTA:se puede meter una funcion o metodo dentro de otra misma,si se pone igual a un objeto
+// ya creado de esa clase aparte de tener los valores tambien tiene acceso a sus metodos que esten disponibles
+
+public void mostrarOrden(){
+    System.out.println("Id_orden: "+this.idOrden);
+    var totalOrden=this.calcularTotal();
+    System.out.println("\t Total de la Orden: $: "+totalOrden);
+    System.out.println("\t Productos de la orden: ");
+    for (int i = 0; i <this.contadorProductos; i++) {
+        System.out.println("\t\t "+this.productos[i]);
+    }
+}
+// TAMBIEN SE PUEDE MOSTRAR CON EL TOSTRING 
+
+@Override
+public String toString(){   
+    
+    var resultado="Id_orden: "+this.idOrden+"\n";
+    var totalOrden=this.calcularTotal();
+ resultado+="\t Total de la Orden: $: "+totalOrden+"\n";
+    resultado+="\t Productos de la orden: "+"\n";
+    for (int i = 0; i <this.contadorProductos; i++) {
+        resultado+="\t\t "+this.productos[i]+"\n";}
+return resultado;
+// para concadenar una cadena puedes usar += asi como cuando concadenas valores o los incrementas
+// en enteros
+} }
--- src/RetoSistemaVentas/ventas/PruebaVentas.java
+++ src/RetoSistemaVentas/ventas/PruebaVentas.java
@@ -4,8 +4,27 @@ public class PruebaVentas {
 public static void main(String[] args) {
     System.out.println("***Sistema de ventas****");
     var producto1=new Producto("Blusa",30.25);
-    System.out.println(producto1.toString());
+    // System.out.println(producto1.toString());
     var producto2 =new Producto("Zapatos",50.00);
-    System.out.println(producto2.toString());
+    // System.out.println(producto2.toString());
+
+// PRIMERA ORDEN
+var orden1=new Orden();
+orden1.agregarProducto(producto1);
+orden1.agregarProducto(producto2);
+// orden1.mostrarOrden();
+System.out.println(orden1);
+
+// Segunda orden
+var orden2=new Orden();
+orden2.agregarProducto(new Producto("Playera",15.75));
+orden2.agregarProducto(producto1);
+orden2.agregarProducto(producto2);
+// orden2.mostrarOrden();
+
+// TAMBIEN SE PUEDE MOSTRAR CON EL TOSTRING 
+System.out.println(orden2);
+
+
 }
 }
